c04211691fbc25e941b0a73bce619778f9b40a71
Abah Dominic <constantx003@gmail.com>
2025-09-21 12:50:16 +0100

    Revert back to cors

--- server.js
+++ server.js
@@ -11,20 +11,20 @@ dotenv.config()
 app.use(express.json()); // Parse JSON bodies
 // app.use(express.urlencoded({ extended: true })); // Parse URL-encoded data if using forms
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-        'Access-Control-Allow-Header',
-        'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
-    );
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    next();
-});
-// app.use(cors({
-//     origin: '*',
-//     methods: ["GET", "POST", "UPDATE", "DELETE"],
-//     allowedHeaders: ["Origin", "X-Requested-Width", "Content-Type", "Z-Keys"]
-// }))
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader(
+//         'Access-Control-Allow-Header',
+//         'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+//     );
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//     next();
+// });
+app.use(cors({
+    origin: '*',
+    methods: ["GET", "POST", "UPDATE", "DELETE"],
+    allowedHeaders: ["Origin", "X-Requested-Width", "Content-Type", "Z-Keys"]
+}))
 app.use('/', router)
 
 mongodb.initDatabase((err) => {
